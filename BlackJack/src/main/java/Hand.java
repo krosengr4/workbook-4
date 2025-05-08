742138a97f5005dd45fdc87aba8677310d52eedb
krosengr4 <rosenkev4@gmail.com>
2025-05-08 11:55:30 -0500

    Set up Hand class for BlackJack exercise

--- BlackJack/src/main/java/Hand.java
+++ BlackJack/src/main/java/Hand.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+
 public class Hand {
+
+    private ArrayList<Card> cards;
+
+    public Hand() {
+        cards = new ArrayList<>();
+    }
+
+    public void deal (Card card) {
+        cards.add(card);
+    }
+
+    public int getSize() {
+        return cards.size();
+    }
+
+    public int getValue() {
+        int value = 0;
+
+        for (Card card : cards) {
+            card.flip();
+            value += card.getPointValue();
+            card.flip();
+        }
+
+        return value;
+    }
 }
