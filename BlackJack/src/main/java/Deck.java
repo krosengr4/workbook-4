d1b7a80da16fb6dc0faed314ab91bc02f411061c
krosengr4 <rosenkev4@gmail.com>
2025-05-08 13:23:36 -0500

    Set up the Deck class for BlackJack exercise

--- BlackJack/src/main/java/Deck.java
+++ BlackJack/src/main/java/Deck.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck (){
+        cards = new ArrayList<>();
+        String[] suites = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+        for(String suite: suites) {
+            for (String value: values) {
+                Card card = new Card(suite, value);
+                cards.add(card);
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card deal() {
+        if(!cards.isEmpty()) {
+            return cards.remove(0);
+        } else {
+            return null;
+        }
+    }
+
+    public int getSize() {
+        return cards.size();
+    }
+
 }
